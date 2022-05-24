45fd55038945fce800a327f3cee0dd7e7062c18f
Sonia Escribano <sescribano@salesforce.com>
2022-05-24 07:52:55 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
-    <link rel="stylesheet" type="text/css" href="https://security-platform-2022-demo--preprod.my.salesforce.com/prefcenter/preference-center-0.0.3/css/pref-center-slds.css">
+        <link rel="stylesheet" type="text/css" href="https://security-platform-2022-demo--preprod.my.salesforce.com/prefcenter/preference-center-0.0.3/css/pref-center-slds.css" />
       </Head>
 
       <main>
@@ -16,8 +16,15 @@ export default function Home() {
         <p className="description">
           Enter your preferences!
         </p>
-
+    
+   
       </main>
+
+      <div id="pref-center-main"></div>
+<script id="preference-center" src="https://security-platform-2022-demo--preprod.my.salesforce.com/prefcenter/preference-center-0.0.3/js/main.js" 
+  pref-center-host="https://security-platform-2022-demo--preprod.my.salesforce.com"
+  pref-center-name="Person_Account_Consent"
+  pref-center-param="pctoken"></script>
 
       <Footer />
     </div>
